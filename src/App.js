46abec0ad72b3b0e450c877f8ed5e3e3c6581baf
JasserChaaben <chaabnjasser@gmail.com>
2024-09-09 20:00:25 +0100

    added images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-
+import {  BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header';
+import DiningRoom from './Pages/Diningroom';
+import Hall from './Pages/Hall';
+import LivingRoom from './Pages/Livingroom';
+import Kitchen from './Pages/Kitchen';
 
 function App() {
   return (
     <div className="App">
-     
+    <Router> 
+     <Header/>
+     <Routes>
+      <Route path='/' element={<Hall/>}/>
+      <Route path='/hall' element={<Hall/>}/>
+      <Route path='/Kitchen' element={<Kitchen/>}/>
+      <Route path='/Diningroom' element={<DiningRoom/>}/>
+      <Route path='/livingroom' element={<LivingRoom/>}/>
+     </Routes>
+     </Router>
     </div>
   );
 }
